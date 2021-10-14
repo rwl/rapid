dbaac8b6b78ce3de873d64b8ad7f60e4d4fbb2aa
Allu, Srikanth <allus@ornl.gov>
2021-10-14 00:38:44 -0400

    adding right extensions

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ release = 'v1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.bibtex']
+extensions = ['sphinxcontrib-bibtex']
 bibtex_bibfiles = ['parareal.bib']
 
 # Add any paths that contain templates here, relative to this directory.
